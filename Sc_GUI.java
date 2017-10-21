287a6a3f92ea36e3aa48063bf04f2713b6210619
Chandan Shastri <crshastri@gmail.com>
2017-10-21 22:31:49 +0530

    Chandan Shastri Atom TEST

--- Sc_GUI.java
+++ Sc_GUI.java
@@ -2,36 +2,36 @@ package Sc_GUI_Java;
 
 import java.awt.Color;
 
-import javax.swing.*;  
-  
+import javax.swing.*;
 
 
-public class Sc_GUI {  
-	
+
+public class Sc_GUI {
+
 public static void main(String[] args) {
-	
-	
-	
-	
+
+
+
+
 	SwingUtilities.invokeLater(new Runnable()
-			
+
 			{
 
-		public void run(){	
+		public void run(){
 			JFrame GUI= new GUI_Manager("ChandanShastri");
 			GUI.setSize(400,400);
 			GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
+
 			GUI.setVisible(true);
 		}
-		
+
 	});
 
-	
-	
-	
-}
-	
-  
 
-}  
+	////
+
+}
+
+
+
+}
